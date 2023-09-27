85e0ec92ed345e35d2e50dcbc022898febcedc2f
MohamadReza Moghadasi <30479983+mrmoghadasi@users.noreply.github.com>
2023-09-27 13:27:45 +0330

    Update csf_status.sh

--- csf_status.sh
+++ csf_status.sh
@@ -3,7 +3,7 @@
 csf_status=$(sudo csf -l | grep "csf and lfd have been disabled")
 
 if [ -n "$csf_status" ]; then
-  echo "csf_status 1" > /var/lib/node_exporter/textfile_collector/csf.prom
-else
   echo "csf_status 0" > /var/lib/node_exporter/textfile_collector/csf.prom
+else
+  echo "csf_status 1" > /var/lib/node_exporter/textfile_collector/csf.prom
 fi
